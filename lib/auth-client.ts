379b6c226a5ac5b0c9e20af3fb1c41b794b30a6f
William Janssen <janssenwilliam02@gmail.com>
2025-10-19 15:32:55 +0200

    Following Cosden SOlutions server actions

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,9 +1,9 @@
-"use client";
+// "use client";
 
-import { createAuthClient } from "better-auth/react";
-export const authClient = createAuthClient({
-  /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "http://localhost:3000",
-});
+// import { createAuthClient } from "better-auth/react";
+// export const authClient = createAuthClient({
+//   /** The base URL of the server (optional if you're using the same domain) */
+//   baseURL: "http://localhost:3000",
+// });
 
-export const { useSession, signIn, signUp, signOut } = authClient;
+// export const { useSession, signIn, signUp, signOut } = authClient;
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,12 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { db, client } from "@/db"; // Mongo Db and client
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, { client }),
   emailAndPassword: {
     enabled: true,
   },
+  plugins: [nextCookies()],
 });
